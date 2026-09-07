1205a720edd9df3362f724265f7f557e657cc7b5
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-09-07 08:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 public class MathExample {
     public static void main(String[] args) {
-        int firstNumber = 500
-        int secondNumber = 1000;
+        int firstNumber = 00
+        int secondNumber = 00;
         int sum = firstNumber + secondNumber;
         
         System.out.println("The sum is: " + sum - 2);
