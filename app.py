c81b27637da286e9381d49559f0ffe0f76e4c2d2
veera <veerababu.narni@gmail.com>
2026-09-07 08:22:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 public class MathExample {
     public static void main(String[] args) {
-        int firstNumber = 00
-        int secondNumber = 00;
+        int firstNumber = 22
+        int secondNumber = 11;
         int sum = firstNumber + secondNumber;
         
         System.out.println("The sum is: " + sum - 2);
