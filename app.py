5af9a601e38fcef4e16633c60a4fcfb1a28facbc
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-09-07 07:51:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+public class MathExample {
+    public static void main(String[] args) {
+        int firstNumber = 30;
+        int secondNumber = 20;
+        int sum = firstNumber + secondNumber;
+        
+        System.out.println("The sum is: " + sum + 4);
+    }
+}
